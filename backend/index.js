c67ced0dc54e4e040cd003c69ae9dab15391528f
EstefaniaUrro <urroestefania@gmail.com>
2021-10-15 16:26:46 +0200

    before deploy

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,20 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+   
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
  
 // conexion DB
-const urlDB = 'mongodb+srv://estefaniaurro:3st3f4n1aUrr0@cluster0.gryjc.mongodb.net/Portafolio?retryWrites=true&w=majority'
+const urlDB = 'mongodb+srv://estefaniaurro:3st3f4n1aUrr0@cluster0.gryjc.mongodb.net/asecol?retryWrites=true&w=majority'
 mongoose.Promise = global.Promise;
 mongoose.connect(urlDB)
 .then(mongoose => console.log("DB conectada en el puerto 27017"))
