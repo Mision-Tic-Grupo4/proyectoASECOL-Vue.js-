e0cec68866a72ee66765f81de3a2625b6f80649a
Cristian Tapasco <tapcrt@gmail.com>
2021-10-14 22:56:37 -0500

    Delete backend directory

--- backend/models/index.js
+++
@@ -1,9 +0,0 @@
-const Usuario = require('./usuario')
-const Categoria = require('./categoria')
-const Producto = require('./producto')
-
-module.exports = {
-    Usuario,
-    Categoria,
-    Producto
-}
